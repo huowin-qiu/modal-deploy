f74db01dead9c857b8576483d8070b47e55b4ef0
vevc <dahaimaomi@gmail.com>
2026-04-02 17:07:14 +0800

    feat: add main entry file

---
+++ main.py
@@ -0,0 +1,47 @@
+import modal
+import subprocess
+import os
+import base64
+
+app = modal.App("vevc-app")
+vevc_image = (
+    modal.Image.debian_slim()
+        .apt_install("curl", "unzip", "supervisor", "procps")
+        .run_commands("curl -sSL https://raw.githubusercontent.com/vevc/modal-deploy/refs/heads/main/install.sh | bash")
+        .pip_install("fastapi[standard]")
+)
+
+_supervisor_started = False
+
+def start_supervisor():
+    global _supervisor_started
+    if not _supervisor_started:
+        subprocess.run(["supervisord"], env=os.environ.copy())
+        _supervisor_started = True
+
+@app.function(
+    image=vevc_image,
+    secrets=[modal.Secret.from_name("custom-secret")],
+    min_containers=1,
+    max_containers=1
+)
+@modal.asgi_app()
+def main():
+    from fastapi import FastAPI
+    from fastapi.responses import PlainTextResponse
+    web_app = FastAPI()
+    uuid = os.environ["U"]
+
+    @web_app.get("/status", response_class=PlainTextResponse)
+    async def status():
+        start_supervisor()
+        return "UP"
+
+    @web_app.get(f"/{uuid}", response_class=PlainTextResponse)
+    async def sub():
+        start_supervisor()
+        domain = os.environ["D"]
+        sub_url = f"vless://{uuid}@{domain}:443?encryption=none&security=tls&sni={domain}&fp=chrome&insecure=0&allowInsecure=0&type=ws&host={domain}&path=%2F%3Fed%3D2560#modal-ws-argo"
+        return base64.b64encode(sub_url.encode("utf-8"))
+
+    return web_app
